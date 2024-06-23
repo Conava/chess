b784eb645919d367bf83ccd03b9ebd14eeea2b2f
2kranz <marlon.kranz@studium.uni-hamburg.de>
2024-06-23 19:18:45 +0200

    Added Testfile for API Methods

---
+++ src/test/java/ptp/project/ChessTest.java
@@ -0,0 +1,68 @@
+package ptp.project;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChessTest {
+    Chess chess;
+
+    @BeforeEach
+    void setUp() {
+        chess = new Chess();
+    }
+
+    @Test
+    void switchToGame() {
+    }
+
+    @Test
+    void switchToMenu() {
+    }
+
+    @Test
+    void openSettingsWindow() {
+    }
+
+    @Test
+    void getGame() {
+    }
+
+
+    @Test
+    void startGame() {
+    }
+
+    @Test
+    void endGame() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void getPlayerWhite() {
+    }
+
+    @Test
+    void getPlayerBlack() {
+    }
+
+    @Test
+    void getPieceAt() {
+    }
+
+    @Test
+    void getLegalSquares() {
+    }
+
+    @Test
+    void getMoveList() {
+    }
+
+    @Test
+    void movePiece() {
+    }
+}
