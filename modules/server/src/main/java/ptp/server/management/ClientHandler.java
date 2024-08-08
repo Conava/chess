4d84d1942ed7e482476a1c3e4fc946e8b68a9b71
2kranz <marlon.kranz@studium.uni-hamburg.de>
2024-08-08 16:57:54 +0200

    Client Handler rework. Now accepts 40 concurrent games. Game creation rework.

--- modules/server/src/main/java/ptp/server/management/ClientHandler.java
+++ modules/server/src/main/java/ptp/server/management/ClientHandler.java
@@ -1,30 +1,41 @@
 package ptp.server.management;
 
-import ptp.core.logic.game.ServerGame;
 import ptp.core.data.io.Message;
+import ptp.core.data.io.MessageType;
 import ptp.server.io.MessageHandler;
 import ptp.core.data.io.MessageParser;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Map;
+import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ClientHandler implements Runnable {
     private final Socket clientSocket;
     private final MessageParser messageParser;
     private final MessageHandler messageHandler;
+    private final Semaphore gameSemaphore;
+    private final Map<Integer, GameInstance> gamesList;
+    private final Map<ClientHandler, Integer> connectionsList;
+    private final AtomicInteger gameIdCounter;
     private PrintWriter out;
     private static final Logger logger = Logger.getLogger(ClientHandler.class.getName());
 
-    public ClientHandler(Socket clientSocket, MessageParser messageParser, Map<GameInstance, ClientHandler[]> games) {
+    public ClientHandler(Socket clientSocket, MessageParser messageParser, Map<Integer, GameInstance> gamesList, Map<ClientHandler, Integer> connectionsList, Semaphore gameSemaphore, AtomicInteger gameIdCounter) {
         this.clientSocket = clientSocket;
         this.messageParser = messageParser;
-        this.messageHandler = new MessageHandler(games);
+        this.messageHandler = new MessageHandler(gamesList, connectionsList);
+        this.gameSemaphore = gameSemaphore;
+        this.gamesList = gamesList;
+        this.connectionsList = connectionsList;
+        this.gameIdCounter = gameIdCounter;
     }
 
     @Override
@@ -35,10 +46,20 @@ public class ClientHandler implements Runnable {
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
                 Message message = messageParser.parse(inputLine);
-                messageHandler.handleMessage(this, message);
+                if (message.getType() == MessageType.CREATE_GAME) {
+                    if (tryCreateGame()) {
+                        sendMessage(new Message(MessageType.SUCCESS, "Game created successfully"));
+                    } else {
+                        sendMessage(new Message(MessageType.ERROR, "Failed to create game. Maximum limit reached."));
+                    }
+                } else {
+                    messageHandler.handleMessage(this, message);
+                }
             }
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Error handling client connection", e);
+        } finally {
+            releaseGameSlot();
         }
     }
 
@@ -47,6 +68,25 @@ public class ClientHandler implements Runnable {
             out.println(messageParser.serialize(message));
         } else {
             logger.log(Level.WARNING, "Output stream is not initialized");
+        }
+    }
+
+    public boolean tryCreateGame() {
+        if (gameSemaphore.tryAcquire()) {
+            int gameId = gameIdCounter.incrementAndGet();
+            GameInstance gameInstance = new GameInstance();
+            gamesList.put(gameId, gameInstance);
+            connectionsList.put(this, gameId);
+            return true;
+        }
+        return false;
+    }
+
+    public void releaseGameSlot() {
+        Integer gameId = connectionsList.remove(this);
+        if (gameId != null) {
+            gamesList.remove(gameId);
+            gameSemaphore.release();
         }
     }
 }
