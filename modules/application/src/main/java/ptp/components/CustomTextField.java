4bda2d0662de9f8b40f1170c1f7b11c10d5af46b
2kranz <marlon.kranz@studium.uni-hamburg.de>
2024-08-08 17:49:55 +0200

    CustomTextField highlight and deactivation visuals

--- modules/application/src/main/java/ptp/components/CustomTextField.java
+++ modules/application/src/main/java/ptp/components/CustomTextField.java
@@ -2,11 +2,15 @@ package ptp.components;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
-public class CustomTextField extends JTextField {
+public class CustomTextField extends JTextField implements FocusListener {
     private ColorScheme colorScheme;
     private static final int ARC_WIDTH = 15;
     private static final int ARC_HEIGHT = 15;
+    private boolean isFieldEnabled = true;
+    private boolean isFieldActive = false;
 
     public CustomTextField(ColorScheme colorScheme) {
         super();
@@ -20,6 +24,14 @@ public class CustomTextField extends JTextField {
         setBackground(colorScheme.getBackgroundColor());
         setCaretColor(colorScheme.getFontColor());
         setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        addFocusListener(this);
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        isFieldEnabled = enabled;
+        repaint();
     }
 
     @Override
@@ -28,6 +40,9 @@ public class CustomTextField extends JTextField {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setColor(getBackground());
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), ARC_WIDTH, ARC_HEIGHT);
+        if (!isFieldEnabled) {
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f));
+        }
         super.paintComponent(g2);
         g2.dispose();
     }
@@ -37,8 +52,23 @@ public class CustomTextField extends JTextField {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setStroke(new BasicStroke(1.2f));
-        g2.setColor(colorScheme.getBorderColor());
+        g2.setColor(isFieldActive ? colorScheme.getAccentColor() : colorScheme.getBorderColor());
+        if (!isFieldEnabled) {
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.1f));
+        }
         g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, ARC_WIDTH, ARC_HEIGHT);
         g2.dispose();
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        isFieldActive = true;
+        repaint();
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        isFieldActive = false;
+        repaint();
     }
 }
